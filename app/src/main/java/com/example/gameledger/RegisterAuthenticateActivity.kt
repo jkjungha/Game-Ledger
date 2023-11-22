6833c2658e9bcdde046fd433fd70ad97c4774c84
jungha <jkjungha24@gmail.com>
2023-11-22 20:29:12 +0900

    settings init

---
+++ app/src/main/java/com/example/gameledger/RegisterAuthenticateActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gameledger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterAuthenticateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_authenticate)
+    }
+}
