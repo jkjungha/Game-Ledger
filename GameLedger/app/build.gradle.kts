042253a5731fb22113af3dcd45933d7e89571b11
jkjungha <108046448+jkjungha@users.noreply.github.com>
2023-11-30 08:51:44 +0900

    feat database settings

--- GameLedger/app/build.gradle.kts
+++ GameLedger/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("org.jetbrains.kotlin.android")
 }
 
@@ -40,7 +41,9 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.google.firebase:firebase-database-ktx")
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
