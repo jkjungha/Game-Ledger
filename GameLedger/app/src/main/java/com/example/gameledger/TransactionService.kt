83e86f19d7ccae5defa057779f756ff61fb520dc
zzoo9ri <nohapril11@gmail.com>
2024-04-29 11:56:11 +0900

    add transaction interface

---
+++ GameLedger/app/src/main/java/com/example/gameledger/TransactionService.kt
@@ -0,0 +1,35 @@
+package com.example.gameledger
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface TransactionService {
+    @FormUrlEncoded
+    @POST("/input/info")
+    fun inputInfoData(
+        @Field("transYear") transYear: Int,
+        @Field("transMonth") transMonth: Int,
+        @Field("transDay") transDay: Int,
+        @Field("transCategory") transCategory: String,
+        @Field("transName") transName: String,
+        @Field("transValue") transValue: Int,
+        @Field("transType") transType: Boolean
+    ): Call<ResponseBody>
+
+    @GET("/list/info")
+    fun listInfoData(
+        @Header("token") authorization: String
+    ): Call<List<TransactionInfo>>
+
+    @GET("/list/info")
+    fun totalInfoData(
+        @Header("token") authorization: String
+    ): Call<TotalInfo>
+
+}
