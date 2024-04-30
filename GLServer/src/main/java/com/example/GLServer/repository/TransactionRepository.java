efc0c91b9904551cfa4d14561aae5f312c7a4723
jkjungha <108046448+jkjungha@users.noreply.github.com>
2024-05-01 01:52:16 +0900

    edit server

--- GLServer/src/main/java/com/example/GLServer/repository/TransactionRepository.java
+++ GLServer/src/main/java/com/example/GLServer/repository/TransactionRepository.java
@@ -9,11 +9,11 @@ import java.util.Optional;
 
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Integer> {
     @Query("SELECT SUM(t.tranValue) FROM TransactionEntity t WHERE t.userEntity.username = ?1 AND t.dateEntity.year = ?2 AND t.dateEntity.month = ?3 AND t.tranType = ?4")
-    Optional<Double> sumTranValueByUserEntityAndDateEntityAndTranType(String username,int year, int month, Boolean tranType);
+    Double sumTranValueByUserEntityAndDateEntityAndTranType(String username,int year, int month, Boolean tranType);
 
     @Query("SELECT t FROM TransactionEntity t WHERE t.userEntity.username = ?1 AND t.dateEntity.year = ?2 AND t.dateEntity.month = ?3")
     Optional<List<TransactionEntity>> findAllByUserEntityAndDateEntity(String username, int year, int month);
 
     @Query("SELECT SUM(t.tranValue) FROM TransactionEntity t WHERE t.userEntity.username = ?1 AND t.dateEntity.year = ?2 AND t.dateEntity.month = ?3 AND t.tranType = ?4 AND t.tranCategory = ?5")
-    Optional<Double> sumTranValueByUserEntityAndDateEntityAndCategory(String username, int year, int month, Boolean tranType, String tranCategory);
+    Double sumTranValueByUserEntityAndDateEntityAndCategory(String username, int year, int month, Boolean tranType, String tranCategory);
 }
