9baa993f484b6399403d9ffccd152fa145e1a268
zzoo9ri <nohapril11@gmail.com>
2024-06-03 23:50:39 +0900

    add quest animation, edit showlist layout

--- GameLedger/app/src/main/java/com/example/gameledger/QuestAdapter.kt
+++ GameLedger/app/src/main/java/com/example/gameledger/QuestAdapter.kt
@@ -1,10 +1,14 @@
 package com.example.gameledger
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.lottie.Lottie
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 
 class QuestAdapter(val questList:ArrayList<Quests>) : RecyclerView.Adapter<QuestAdapter.CustomViewHolder>()
 {
@@ -20,6 +24,22 @@ class QuestAdapter(val questList:ArrayList<Quests>) : RecyclerView.Adapter<Quest
         holder.questTitle.text = questList.get(position).questTitle
         holder.questValue.text = questList.get(position).questValue
         holder.accumulatedAmount.text = questList.get(position).accumulatedAmount
+
+        if(questList.get(position).questCategory == "식비") {
+            holder.categoryImg.setAnimation(R.raw.monster_1)
+        }
+        else if(questList.get(position).questCategory == "교통") {
+            holder.categoryImg.setAnimation(R.raw.monster_5)
+        }
+        else if(questList.get(position).questCategory == "문화") {
+            holder.categoryImg.setAnimation(R.raw.monster_3)
+        }
+        else if(questList.get(position).questCategory == "생활") {
+            holder.categoryImg.setAnimation(R.raw.monster_4)
+        }
+
+        holder.categoryImg.repeatCount = LottieDrawable.INFINITE
+        holder.categoryImg.playAnimation()
     }
 
     override fun getItemCount(): Int {
@@ -27,6 +47,7 @@ class QuestAdapter(val questList:ArrayList<Quests>) : RecyclerView.Adapter<Quest
     }
 
     class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val categoryImg = itemView.findViewById<LottieAnimationView>(R.id.lav_quest)    // 카테고리별 애니메이션
         val questCategory = itemView.findViewById<TextView>(R.id.tv_questCategory)    // 카테고리
         val questTitle = itemView.findViewById<TextView>(R.id.tv_quest)    // 퀘스트
         val questValue = itemView.findViewById<TextView>(R.id.tv_questValue)    // 퀘스트 목표 금액
